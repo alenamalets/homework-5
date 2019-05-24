40ac1c3cfd7e4792462871887090eeab112425b2
Alena <elen.malets@gmail.com>
2019-05-24 22:42:50 +0200

    create express app

---
+++ messages-api.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const port = 3000
+let cnt = 0
+app.use(bodyParser.json())
+app.post('/messages', (req, res) => {
+    if (cnt >= 5) {
+        return res.status(500).send()
+    }
+    if (!req.body.text) {
+        return res.status(400).send()
+    }
+    else {
+        cnt++
+        console.log(req.body.text)
+    }
+
+})
+app.listen(port, () => console.log(`Listening on port ${port}!`))
