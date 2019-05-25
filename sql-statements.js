fe7377afaa72155510f6a3c7d4ded95ee95ef644
Alena <elen.malets@gmail.com>
2019-05-25 10:23:25 +0200

    update and select person

--- sql-statements.js
+++ sql-statements.js
@@ -12,7 +12,7 @@ pool.on('error', (err) => {
 })
 
 pool.query(` 
-    CREATE TABLE IF NOT EXISTS person 
+    CREATE TABLE IF NOT EXISTS "person" 
     (
         id serial, 
         first_name varchar(255), 
@@ -28,12 +28,49 @@ pool.query(`
 app.use(bodyParser.json())
 
 app.post('/person', (req, res, next) => {
-    pool.query('INSERT INTO person (first_name, last_name, eye_color) VALUES ($1, $2, $3) RETURNING *',
+    pool.query('INSERT INTO "person" (first_name, last_name, eye_color) VALUES ($1, $2, $3) RETURNING *',
         [req.body.first_name,
         req.body.last_name,
         req.body.eye_color])
         .then(results => res.json(results.rows[0]))
-        .catch(next)
+        .catch(err => {
+            console.log(err);
+            return next(err)}
+        )
 })
+
+app.put('/person/update', (req, res, next) => {
+    pool.query(`UPDATE "person" SET eye_color = 'blue eyes' WHERE eye_color = 'brown eyes' RETURNING *`)
+        .then(results => {
+            if (results.rowCount === 0) {
+                res.status(404).end()
+            } else {
+                res.json(results.rows)
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            return next(err)}
+        )
+})
+
+app.get('/person/select', (req, res, next) => {
+    const name = "James";
+    pool.query(`SELECT * FROM "person" WHERE first_name = $1`, [name])
+        .then(results => {
+            if (results.rowCount === 0) {
+                res.status(404).end()
+            } else {
+                res.json(results.rows)
+                console.log(results.rows);
+                
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            return next(err)}
+        )
+})
+
 
 app.listen(port, () => console.log("listening on port " + port))
