0d3e7dabca2fa386948414942fbf935f1a225ecd
Alena <elen.malets@gmail.com>
2019-05-25 11:00:38 +0200

    create movie

---
+++ sequelize-rest.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const Sequelize = require('sequelize')
+
+const port = 3000
+const sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres', { define: { timestamps: false } })
+
+const Movie = sequelize.define('movie', {
+    title: Sequelize.TEXT,
+    yearOfRelease: Sequelize.INTEGER,
+    synopsis: Sequelize.TEXT
+}, {
+        tableName: 'movie'
+    })
+
+Movie.sync()
+
+app.use(bodyParser.json())
+
+app.post('/movie', (req, res, next) => {
+    Movie
+        .create(req.body)
+        .then(movie => {
+            if (!movie) {
+                return res.status(404).send({
+                    message: `Movie does not exist`
+                })
+            }
+            return res.status(201).send(movie)
+        })
+        .catch(error => next(error))
+})
+
+app.listen(port, () => `Listening on port ${port}`)
