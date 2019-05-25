60affaf8afde44ff58c36ecc5aae052460f7be9d
Alena <elen.malets@gmail.com>
2019-05-25 11:43:00 +0200

    all movies and pagination

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -18,6 +18,7 @@ Movie.sync()
 
 app.use(bodyParser.json())
 
+//_create_ a new movie resource
 app.post('/movie', (req, res, next) => {
     Movie
         .create(req.body)
@@ -32,6 +33,7 @@ app.post('/movie', (req, res, next) => {
         .catch(error => next(error))
 })
 
+//_read_ a single movie resource
 app.get('/movie/:id', (req, res, next) => {
     Movie
       .findByPk(req.params.id)
@@ -46,6 +48,7 @@ app.get('/movie/:id', (req, res, next) => {
       .catch(error => next(error))
 })
 
+//_update_ a single movie resource
 app.put('/movie/:id', (req, res, next) => {
     Movie
       .findByPk(req.params.id)
@@ -59,5 +62,38 @@ app.put('/movie/:id', (req, res, next) => {
       })
       .catch(error => next(error))
 })
+
+//_delete_ a single movie resource
+app.delete('/movie/:id', (req, res, next) => {
+    Movie
+      .findByPk(req.params.id)
+      .then(movie => {
+        if (!movie) {
+          return res.status(404).send({
+            message: `Movie does not exist`
+          })
+        }
+        return movie.destroy()
+          .then(() => res.send({
+            message: `Movie was deleted`
+          }))
+      })
+      .catch(error => next(error))
+})
+
+//_read_ all movies (the entire collections resource)
+app.get('/movie', (req, res, next) => {
+    const limit = req.query.limit || 3
+    const offset = req.query.offset || 0
+    Movie
+    .findAll({
+        limit, offset
+      })
+      .then(movies => {
+        res.send({ movies })
+      })
+      .catch(error => next(error))
+})
+
 
 app.listen(port, () => `Listening on port ${port}`)
